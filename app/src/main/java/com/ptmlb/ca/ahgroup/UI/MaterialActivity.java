23af531b816a861db9476f99923a409b66a77f06
fmisser-workpc <fmisser@vip.qq.com>
2015-12-26 19:24:43 +0800

    material design demo

---
+++ app/src/main/java/com/ptmlb/ca/ahgroup/UI/MaterialActivity.java
@@ -0,0 +1,28 @@
+package com.ptmlb.ca.ahgroup.ui;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.ptmlb.ca.ahgroup.R;
+
+public class MaterialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material);
+
+        Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.bottomsheet_camera);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout);
+        collapsingToolbarLayout.setTitle("详情界面");
+    }
+}
